9f8392421817947497b6de8aac45dfb9212f5789
gtait15 <gtait015@gmail.com>
2025-02-19 15:14:58 -0800

    adjusted placement of line breaks and created new method to clean up the files

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 # This is a sample Python script.
-import nltk
 from nltk import word_tokenize
-nltk.download('punkt_tab')
 
-#Press Shift+F10 to execute it or replace it with your code.
-# Press Ddef print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
+def clean_file(input_file, output_file):
+    with open(input_file, 'r') as original:
+        with open(output_file, 'w') as cleaned:
+            for line in original:
+                tokens = word_tokenize(line)
+                in_brackets = 0
+                for token in tokens:
+                    if token == "[":
+                         in_brackets = 1
+                    elif token == "]":
+                         in_brackets = 0
+                    if not in_brackets and token.isalnum():
+                        token = token.lower()
+                        if token == "and" or token == "but" or token == "so": #
+                             cleaned.write('\n')
+                        cleaned.write(token)
+                        cleaned.write(" ")
 
 
 # # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    with open('SC349_CP_Original.txt', 'r', encoding="utf-8") as original:
-        with open('SC349_CP_Cleaned.txt', 'w') as cleaned:
-            word_count = 0
-            for line in original:
-                tokens = word_tokenize(line)
-                ignored = 0
-                for token in tokens:
-                    token.lower()
-                    if token == "[":
-                         ignored = 1
-                    elif token == "]":
-                         ignored = 0
-                    if not ignored and token.isalnum():
-                        word_count += 1
-                        cleaned.write(token)
-                        cleaned.write(" ")
-                        if word_count > 16:
-                             cleaned.write('\n')
-                             word_count = 0
-
-
+    clean_file('SC349_CoP_Original.txt','SC349_CoP_Cleaned.txt')
