4207dcd51907ace695a62b653db178280151e75e
gtait15 <gtait015@gmail.com>
2025-02-24 12:36:59 -0800

    fixed removal of apostrophes, added words to blacklist

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def replace_written_numbers(text):
         "forty-four" : "44",
         "forty-seven" : "47",
         "fifty" : "50",
+        "fifty-one" : "50",
         "sixty" : "60",
         "seventy" : "70",
         "eighty" : "80",
@@ -41,24 +42,30 @@ def replace_written_numbers(text):
 def clean_file(input_file, output_file):
     with open(input_file, 'r') as original:
         with open(output_file, 'w') as cleaned:
-            blacklist = {"F634", "F631", "f634", "f631"} #speaker codes
+           # speaker_codes = {"F634", "F631", "f634", "f631"}
+            black_list = {"f634", "f631", "mm", "mmhm", "em", "erm", "eh", "uh", "uh huh"}
             new_line_trigger = {"and", "but", "so"}
-
             for line in original:
                 tokens = word_tokenize(line)
                 in_brackets = 0
                 for token in tokens:
                     token = token.lower()
                     token = replace_written_numbers(token)
+                    token = token.replace("-"," ")
+                    #token = token.replace("ing", "in") #chaotic neutral way to fix
                     if token == "[":
                          in_brackets = 1
                     elif token == "]":
                          in_brackets = 0
-                    if not in_brackets and token.isalnum() and token not in blacklist:
-                        if token in new_line_trigger: #
+                    if not in_brackets and token not in black_list and token.replace("'","").isalnum():
+                        if token in new_line_trigger: #or token in speaker_codes:
                              cleaned.write('\n')
+                        if token.find('\'') < 0: # add space if not contraction
+                            cleaned.write(" ")
                         cleaned.write(token)
-                        cleaned.write(" ")
+
+                       # if token in speaker_codes:
+                            # cleaned.write('\n')
 
 
 # # Press the green button in the gutter to run the script.
