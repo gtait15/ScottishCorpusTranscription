557d206d8492d8d2d17a4ec36e5e87be0802213e
gtait15 <gtait015@gmail.com>
2025-02-25 14:08:54 -0800

    reorganized code and added to blacklist

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# This is a sample Python script.
 from nltk import word_tokenize
 
-def replace_written_numbers(text):
+def clean_up(text):
+    text = text.lower()
+    text = text.replace("/", "")
+    text = text.replace(".", "")
+    #token = token.replace("ing", "in") #chaotic way to fix
     written_numbers = {
         "one": "1",
         "two": "2",
@@ -11,48 +14,48 @@ def replace_written_numbers(text):
         "six": "6",
         "seven": "7",
         "eight": "8",
-        "nine" : "9",
-        "ten" : "10",
-        "eleven" : "11",
-        "twelve" : "12",
-        "thirteen" : "13",
-        "fourteen" : "14",
-        "fifteen" : "15",
-        "sixteen" : "16",
-        "seventeen" : "17",
-        "eighteen" : "18",
-        'nineteen' : "19",
-        "twenty" : "20",
-        "thirty" : "30",
-        "forty" : "40",
-        "forty-four" : "44",
-        "forty-seven" : "47",
-        "fifty" : "50",
-        "fifty-one" : "50",
-        "sixty" : "60",
-        "seventy" : "70",
-        "eighty" : "80",
-        "ninety" : "90",
-        "thousand" : "1000",
+        "nine": "9",
+        "ten": "10",
+        "eleven": "11",
+        "twelve": "12",
+        "thirteen": "13",
+        "fourteen": "14",
+        "fifteen": "15",
+        "sixteen": "16",
+        "seventeen": "17",
+        "eighteen": "18",
+        'nineteen': "19",
+        "twenty": "20",
+        "twenty-six" : "26",
+        "thirty": "30",
+        "thirty-nine" : "39",
+        "forty": "40",
+        "forty-four": "44",
+        "forty-six": "46",
+        "forty-seven": "47",
+        "fifty": "50",
+        "fifty-one": "50",
+        "sixty": "60",
+        "sixty-nine" : "69",
+        "seventy": "70",
+        "seventy-one" : "71",
+        "eighty": "80",
+        "ninety": "90",
+        "thousand": "1000",
     }
     if text in written_numbers:
         return written_numbers[text]
     return text
 
-def clean_up(text):
-    text = text.lower()
-    text = text.replace_written_numbers(text)
-    text = text.replace("/", "")
-    text = text.replace(".", "")
-    # token = token.replace("ing", "in") #chaotic way to fix
-    return text
+def token_not_punctuation(text):
+   return text.isalnum() or text.replace("'","").isalnum() or text.replace("-", "").isalnum()
 
 def clean_file(input_file, output_file):
     with open(input_file, 'r') as original:
         with open(output_file, 'w') as cleaned:
             black_list = {"f634", "f631", "mm", "mmhm", "em", "erm", "eh", "uh", "uhhuh", "um",
-                          "ehm", "huh", "er", "f1071", "m1070", "f718", "f1077", "m1078"}
-            new_line_trigger = {"and", "but", "so", "no", "yeah", "yes"}
+                          "ehm", "huh", "er", "f1071", "m1070", "f718", "f1077", "m1078", "uh-huh", "hm"}
+            new_line_trigger = {"and", "but", "so", "no", "yeah", "yes", "oh", "well"}
             contraction_endings = {"'nt", "'ll", "'d", "'ve", "'t","'t've","'s","'n","'d", "'re",
                             "'all", "'mon", "n't", "'m"} #nltk inconsistent about tokenizing these for some reason
             contractions = {"didn't", "o'clock", "don't", "i'm", "can't", "isn't", "that's", "you've"}
@@ -65,9 +68,7 @@ def clean_file(input_file, output_file):
                          in_brackets = 1
                     elif token == "]":
                          in_brackets = 0
-                    if not (token.replace("'", "").isalnum() or token.replace("-", "").isalnum()):
-                        print(token)
-                    if not in_brackets and token not in black_list and (token.replace("'","").isalnum() or token.replace("-", "").isalnum()):
+                    if not in_brackets and token not in black_list and token_not_punctuation(token):
                         if token in new_line_trigger:
                              cleaned.write('\n')
                         if token not in contraction_endings:
